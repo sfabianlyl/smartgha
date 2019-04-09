f6896dd27e516294dcda5754988cce3d285ebff0
sfabianlyl <36915473+sfabianlyl@users.noreply.github.com>
2019-04-09 17:01:41 +0800

    Add files via upload

--- login.php
+++ login.php
@@ -11,27 +11,31 @@
 <html>
     <head>
         <link rel="stylesheet" type="text/css" media="screen" href="asset/main.css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:bold" rel="stylesheet"> 
         <script src="asset/main.js"></script>
         <title>Smart Greenhouse Application</title>
+
     </head>
 
 
     <body>
         <form action="" method="POST">
             <div class="login-box">
-                <table class="login-table"><tbody>
-                    <tr>
-                        <td>Username:</td>
-                        <td><input name="username" type="text"/></td>
-                    </tr>
-                    <tr>
-                        <td>Password:</td>
-                        <td><input name="pass" type="password"/></td>
-                    </tr>
-                    <tr>
-                        <td colspan=2 style="text-align:right;"><button type="submit" name="submit" value="submit">Submit</button></td>
-                    </tr>
-                </tbody></table>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>USERNAME:</td>
+                            <td><input name="username" type="text" placeholder="Username"/></td>
+                        </tr>
+                        <tr>
+                            <td>PASSWORD:</td>
+                            <td><input name="pass" type="password" placeholder="Password"/></td>
+                        </tr>
+                        <tr>
+                            <td colspan=2 style="text-align:center;"><button type="submit" name="submit" value="submit">SUBMIT</button></td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </form>
     </body>
